37c73f0d685cf2db66d2b517cf5cd9c9a0bf363a
Wilman Martins <wilmanmartinscl@digitechfp.com>
2026-04-25 02:02:15 +0200

    migracion a Angular. Navbar, footer y estructura de componentes, assets y demas, foto del abyss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LayersComponent } from './pages/layers/layers.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'layers', component: LayersComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
---
+++ src/app/pages/layers/layers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layers',
+  imports: [],
+  templateUrl: './layers.component.html',
+  styleUrl: './layers.component.css'
+})
+export class LayersComponent {
+
+}
