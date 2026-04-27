b779723a46506cfa33ba3bb975c1ad069acfafa2
Wilman Martins <wilmanmartinscl@digitechfp.com>
2026-04-27 23:55:50 +0200

    arreglar un bug con el home, tambien hice deploy del proyecto en github pages

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,5 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+}
