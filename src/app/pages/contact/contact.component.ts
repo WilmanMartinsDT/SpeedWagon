ce69cfb0418e3953978c84353b29a02c89d58f93
Huigen <huigenchen1@gmail.com>
2026-04-28 16:27:07 +0200

    Voy a dejar esto subido. Tengo que aclarar las dudas y arreglar el problema de ngforms. Da error tooooodo el rato

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, NgModule} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// @ts-ignore
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    FormsModule,
+    ContactComponent
+  ]
+})
+export class AppModule {}
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 
+  form = {
+    name: '',
+    email: '',
+    topic: '',
+    date: '',
+    message: '',
+    newsletter: false
+  };
+
+  lastScrollTop = 0;
+  navbarHidden = false;
+
+  onSubmit() {
+    console.log('Form data:', this.form);
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    this.navbarHidden = scrollTop > this.lastScrollTop;
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  }
 }
